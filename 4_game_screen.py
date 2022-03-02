4f0f0809224ce3b10df9faeb0d3bdd3594cc2431
Leanna <pureaa94@gmail.com>
2022-03-02 21:59:42 +0900

    게임 스크린을 구성하는 파일 생성 리스트에 대한 개념 잡기 필요

---
+++ 4_game_screen.py
@@ -0,0 +1,57 @@
+import pygame
+
+
+def display_start_screen():
+    pygame.draw.circle(screen, WHITE, start_button.center, 60, 5)
+
+
+def display_game_screen():
+    print("Game Start")
+
+
+def check_buttons(pos):
+    global start
+    if start_button.collidepoint(pos):
+        start = True
+
+
+pygame.init()
+screen_width = 1280
+screen_height = 720
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Memory Game")
+
+start_button = pygame.Rect(0, 0, 120, 120)
+start_button.center = (120, screen_height - 120)
+
+BLACK = (0, 0, 0)  # RGB
+WHITE = (255, 255, 255)
+
+start = False
+
+running = True
+while running:
+    click_pos = None
+
+    # 이벤트 루프
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONUP:
+            click_pos = pygame.mouse.get_pos()
+            print(click_pos)
+
+    screen.fill(BLACK)
+
+    if start:
+        display_game_screen()
+    else:
+        display_start_screen()
+
+    if click_pos:
+        check_buttons(click_pos)
+
+    pygame.display.update()
+
+
+pygame.quit()
