b7be3c04f0753ee0aeb0121a88b286f33dac52b7
Leanna <pureaa94@gmail.com>
2022-03-02 21:58:37 +0900

    처음부터 다시 시작함

--- 1_frame.py
+++ 1_frame.py
@@ -1,9 +1,9 @@
 import pygame
 
-pygame.init()
+pygame.init() # 선언
 screen_width = 1280
 screen_height = 720
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height)) # 변수 = 함수(크기 설정)
 pygame.display.set_caption("Memory Game")
 
 running = True
